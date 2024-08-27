27e6af57ac2ce37cc28d9da763ba84e35dccf0ce
Sandeep Dhungana <sandeepdhungana10@gmail.com>
2024-08-27 22:11:31 +0530

    added region

--- server.py
+++ server.py
@@ -4,4 +4,4 @@ from app.socket import socketio
 app = create_app()
 
 if __name__ == '__main__':
-    socketio.run(port=5000, app=app, debug=True,host='0.0.0.0')
+    socketio.run(port=5050, app=app, debug=True,host='0.0.0.0')
