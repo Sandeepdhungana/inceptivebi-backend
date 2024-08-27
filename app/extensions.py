588c832cc0e804a0312e7a3e3304e3e6d53f2d94
Sandeep Dhungana <sandeepdhungana10@gmail.com>
2024-08-27 22:05:26 +0530

    first commit

--- app/extensions.py
+++ app/extensions.py
@@ -2,18 +2,7 @@ import boto3
 import os
 
 
-import os
-import boto3
-
-aws_access_key_id 
-
-lex_client = boto3.client(
-    'lexv2-runtime',
-    aws_access_key_id=aws_access_key_id,
-    aws_secret_access_key=aws_secret_access_key,
-    region_name='your-region'  # Replace with your AWS region, e.g., 'us-west-2'
-)
-
+lex_client = boto3.client('lexv2-runtime')
 
 
 def get_dynamodb_resource():
